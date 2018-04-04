82cd8d01d563afb2e2fae207ded0aa170636f8e3
Gayathri Rajamanickam <Gayathri_R15@ad.Infosys.com>
2018-04-04 14:31:56 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {TranslateService} from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+  param = {value: 'Q2 LTMS', browserLang: 'DEFAULT'};
+  translater: TranslateService;
+
+  constructor(translater: TranslateService) {
+    this.translater = translater;
+  }
+
+  ngOnInit() {
+    this.param.browserLang = this.translater.getBrowserLang();
+
+    this.translater.addLangs(['en', 'de']);
+    this.translater.setDefaultLang('en');
+    this.translater.use('en');
+  }
 }
